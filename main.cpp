ad3073b2a0eb42ef3d5ab94f6511e0cea2709119
DarkBlood202 <32721945+DarkBlood202@users.noreply.github.com>
2019-06-03 19:45:59 -0500

    Primeros avances. Pantalla de bienvenida e introducción de datos.

--- main.cpp
+++ main.cpp
@@ -2,9 +2,29 @@
 #include<string.h>
 using namespace std;
 
+struct Usuario{
+    string nombre;
+    string sexo;
+    int edad;
+};
+
 int main(int argc, char** argv){
     
-    cout << "Bienvenid@ a su sistema de salud personal." << endl;
+    Usuario usuarios[10];
     
+    cout << "Bienvenid@ a su sistema de asistencia en salud." << "\n\nVeo que es un usuario nuevo. Por favor, cuentame un poco sobre ti." << endl;
+    
+    //while(!confirmar){
+    
+        cout <<"\nTu nombre, por favor: ";
+        getline(cin,usuarios[0].nombre);
+        
+        cout << "Un gusto, " << usuarios[0].nombre << ".\n\n";
+        
+        cout << "Y dime, " << usuarios[0].nombre << ", tu eres del sexo...: ";
+        getline(cin,usuarios[0].sexo);
+        
+        cout << "Ya veo. Excelente. Eres del sexo " << usuarios[0].sexo << ".\n\n";
+//}
     return 0;
 }
