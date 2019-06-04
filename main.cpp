ef33f0caf693b9639ed081a568a9a47f9ab77d63
DarkBlood202 <32721945+DarkBlood202@users.noreply.github.com>
2019-06-03 23:59:18 -0500

    Terminada entrada de datos del usuario.

--- main.cpp
+++ main.cpp
@@ -11,10 +11,11 @@ struct Usuario{
 int main(int argc, char** argv){
     
     Usuario usuarios[10];
+    bool confirmar_datos = false;
     
     cout << "Bienvenid@ a su sistema de asistencia en salud." << "\n\nVeo que es un usuario nuevo. Por favor, cuentame un poco sobre ti." << endl;
     
-    //while(!confirmar){
+    while(!confirmar_datos){
     
         cout <<"\nTu nombre, por favor: ";
         getline(cin,usuarios[0].nombre);
@@ -27,11 +28,36 @@ int main(int argc, char** argv){
         cout << "Ya veo. Excelente. Eres del sexo " << strlwr(usuarios[0].sexo) << ".\n\n";
 
         cout << "Por ultimo, " << usuarios[0].nombre << " , cual es tu edad: ";
-        cin >> usuarios[0].edad;
+        while(!(cin >> usuarios[0].edad)){
+        	cin.clear();
+        	cin.ignore(100,'\n');
+        	cout << "Uhm... creo que te has confundido, " << usuarios[0].nombre << ".\n";
+        	cout << "Por ultimo, " << usuarios[0].nombre << " , cual es tu edad: ";
+        }
         
         cout << "Oh entiendo. Tienes " << usuarios[0].edad << ".\n\n";
+        
+        cout << "Ok. Recapitulemos, te parece?\nEres " << usuarios[0].nombre;
+		cout << ", del sexo " << usuarios[0].sexo << " y con " << usuarios[0].edad << " abriles.";
+		
+		fflush(stdin);
+		bool validez_respuesta=false;
+		string respuesta_sn;
+		
+		while(!validez_respuesta){
+			cout << "\n\nSon correctos estos datos? (S/N)\nIngrese su respuesta: ";
+			getline(cin,respuesta_sn);
+			if (respuesta_sn == "N" || respuesta_sn == "n"){
+				cout << "\nComprendido. Intentemoslo nuevamente.\n";
+				validez_respuesta=true;
+			}
+			else if(respuesta_sn == "S" || respuesta_sn == "s"){
+				cout << "\nEsplendido. Entonces comencemos.\n";
+				validez_respuesta=true;
+				confirmar_datos=true;
+			}
+		}
 
-
-//}
+	}
     return 0;
 }
